7e28932a00380254a5cf2fd19ac3aa439035e3a3
hanamichixkarito484 <mikkojulianne@gmail.com>
2026-09-15 01:54:05 +0800

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -8,4 +8,4 @@ const SUPABASE_URL = "https://wtknypnwwzxekokggass.supabase.co/rest/v1/";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0a255cG53d3p4ZWtva2dnYXNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkzODAzMDEsImV4cCI6MjEwNDk1NjMwMX0.HjSMOMk3hBdPpKUndpnUfJpvVKvjJuHZcYCVotNeZDE";
 
 // Loaded via the Supabase CDN script included in every HTML page (see <head>)
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = supabase.createClient(https://wtknypnwwzxekokggass.supabase.co/rest/v1/, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0a255cG53d3p4ZWtva2dnYXNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkzODAzMDEsImV4cCI6MjEwNDk1NjMwMX0.HjSMOMk3hBdPpKUndpnUfJpvVKvjJuHZcYCVotNeZDE);
